21914f30993cac345a92bb7d83106a596b613478
Kirbyasdf <kirbyAugustus@gmail.com>
2020-03-22 21:46:46 -0400

    app.locals var

--- dragon-stack/server/app/api/routes/dragon.routes.js
+++ dragon-stack/server/app/api/routes/dragon.routes.js
@@ -3,7 +3,9 @@ const { Router } = require("express");
 const router = new Router();
 
 router.get("/", (req, res, next) => {
-  res.json({ dragon: engine.generation.newDragon() });
+  res.json({ dragon: req.app.locals.engine.generation.newDragon() });
 });
+
+//^ see index.js for explanation of app.locals
 
 module.exports = router;
--- dragon-stack/server/app/index.js
+++ dragon-stack/server/app/index.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 //this(engine) must be above the app.use of dragonRouter since dragonRouter calls the engine...and it wont trigger  if the engine is not already started
 const engine = new GenerationsEngine();
 engine.start();
+app.locals.engine = engine; //allow the engines to become global in the global express app object, see app/api/routes/dragon for example
 
 app.use("/dragon", dragonRouter);
 
