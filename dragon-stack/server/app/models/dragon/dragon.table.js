4c8c7e9cdaec8cde5d3485304ab7adce59bb2b80
Kirbyasdf <47834311+Kirbyasdf@users.noreply.github.com>
2020-03-25 01:19:27 -0400

    Update dragon.table.js

--- dragon-stack/server/app/models/dragon/dragon.table.js
+++ dragon-stack/server/app/models/dragon/dragon.table.js
@@ -10,7 +10,7 @@ class DragonTable {
         'INSERT INTO dragon(nickname, birthdate, "generationId") VALUES($1, $2, $3) RETURNING id',
         [nickname, birthdate, generationId]
       );
-      // notice how generationId is in double quotes...well its to keep the i in Id capped... when doing this you must be inside single qulites so ' "" '
+      // notice how generationId is in double quotes...well its to keep the i in Id capped... when doing this you must be inside single quotes so ' "" '
       const dragonId = resData.rows[0].id;
       const res = await Promise.all(
         dragon.traits.map(({ traitType, traitValue }) => {
